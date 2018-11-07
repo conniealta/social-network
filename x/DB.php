fa405cb3f3791c39dcbc44641f67b0dcd46915d9
Bettina <ba033@hdm-stuttgart.de>
2018-11-07 10:45:32 +0100

    hi

---
+++ x/DB.php
@@ -0,0 +1,20 @@
+<?php
+class DB {
+
+    private static function connect() {
+        $pdo = new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ka034', 'ka034', 'zeeD6athoo', array('charset' => 'utf8'));
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+
+    public static function query($query, $params = array()) {
+        $statement = self::connect()->prepare($query);
+        $statement->execute($params);
+
+        if (explode(' ', $query)[0] == 'SELECT') {
+            $data = $statement->fetchAll();
+            return $data;
+        }
+    }
+
+}
