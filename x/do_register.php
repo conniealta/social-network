2b5a94a7495c6860fa943565b98274e5efd7bf31
conniealta <conniealta@gmail.com>
2018-11-13 10:08:53 +0100

    o

--- x/do_register.php
+++ x/do_register.php
@@ -1,7 +1,5 @@
 
 <?php
-echo("hallo");
-
 session_start();
 
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de;dbname=u-ka034', 'ka034', 'zeeD6athoo',array('charset'=>'utf8'));
@@ -46,7 +44,7 @@ if(isset($_POST["email"]) AND isset($_POST["passwort"])AND isset($_POST["usernam
     //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
     if(!$error) {
         $statement = $pdo->prepare("SELECT * FROM list5 WHERE email = :email");
-        $result = $statement->execute(array('email' => $email));
+        $result = $statement->execute(array(':email' => $email));
         $user = $statement->fetch();
 
         if($user !== false) {
@@ -58,7 +56,7 @@ if(isset($_POST["email"]) AND isset($_POST["passwort"])AND isset($_POST["usernam
     //Überprüfe, dass der Username noch nicht registriert wurde
     if(!$error) {
         $statement = $pdo->prepare("SELECT * FROM list5 WHERE username = :username");
-        $result = $statement->execute(array('username' => $username));
+        $result = $statement->execute(array(':username' => $username));
         $user = $statement->fetch();
 
         if($user !== false) {
@@ -69,13 +67,15 @@ if(isset($_POST["email"]) AND isset($_POST["passwort"])AND isset($_POST["usernam
 
     //Keine Fehler, wir können den Nutzer registrieren
     if(!$error) {
+        echo $passwort;
         $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
 
         $statement = $pdo->prepare("INSERT INTO list5 (username, email, passwort) VALUES (:username, :email, :passwort)");
-        $result = $statement->execute(array('username' => $username, 'email' => $email, 'passwort' => $passwort_hash));
+        echo $passwort_hash;
+        $result = $statement->execute(array(':username' => $username, ':email' => $email, ':passwort'=> hash('sha256', $passwort, false)));
 
         if($result) {
-            echo 'Du wurdest erfolgreich registriert. <a href="login.html">Zum Login</a>';
+            echo 'Du wurdest erfolgreich registriert. <a href="login2.html">Zum Login</a>';
         }
         else {
             echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
@@ -86,8 +86,6 @@ if(isset($_POST["email"]) AND isset($_POST["passwort"])AND isset($_POST["usernam
         }
     }
 }
-
-
 
 
 
